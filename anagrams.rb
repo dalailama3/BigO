21174741a713676f7e84ed3c9ba6dc16d7f1935f
JohnandMike <JohnandMike@appacademy.io>
2015-10-08 14:55:40 -0400

    finish work on anagram methods

--- anagrams.rb
+++ anagrams.rb
@@ -1,4 +1,4 @@
-def first_anagram(string)
+def first_anagram?(string)
   return string if string.length == 2 && string == string.reverse
   result = []
   return (result + [string, string.reverse]) if string.length == 2
@@ -20,4 +20,45 @@ def first_anagram(string)
   result.flatten
 end
 
-p first_anagram("abc")
+# p first_anagram("abc")
+
+def second_anagram(string1, string2)
+  string1.each_char do |char|
+    string1.sub!(char, "") unless string2.empty?
+    string2.sub!(char,"") if string2.include?(char)
+  end
+
+  string1.empty? && string2.empty?
+end
+
+# p second_anagram("elvis", "lives") => true
+# p second_anagram("green", "greene") => false
+# p second_anagram("happens", "happen") => false
+
+def third_anagram(string1, string2)
+  string1.split("").sort == string2.split("").sort
+end
+
+# p third_anagram("elvis", "lives")  # => true
+# p third_anagram("green", "greene") # => false
+# p third_anagram("happens", "happen") # => false
+
+def fourth_anagram(str1, str2)
+  count1 = Hash.new(0)
+  count2 = Hash.new(0)
+  str1.each_char do |char|
+    count1[char] += 1
+  end
+  str2.each_char do |char|
+    count2[char] += 1
+  end
+  count1 == count2
+
+end
+
+
+
+
+p fourth_anagram("elvis", "lives")  # => true
+p fourth_anagram("green", "greene") # => false
+p fourth_anagram("happens", "happen") # => false
